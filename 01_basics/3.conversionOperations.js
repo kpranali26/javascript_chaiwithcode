a480464d62b1f833825696c60392fb891af7181a
kpranali26 <166025877+kpranali26@users.noreply.github.com>
2025-01-07 17:24:00 +0000

    3rd

---
+++ 01_basics/3.conversionOperations.js
@@ -0,0 +1,11 @@
+let score ="123abd"
+
+console.log(typeof score)
+
+let valNumber = Number(score)
+console.log(typeof valNumber)
+console.log( valNumber) //
+// true=1 false = 0
+// empty string => "" when we convert string to Boolean
+// "123abc" is a NaN
+
