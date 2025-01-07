6f207a70e48ddde833bc73e6026c7a93499f3900
kpranali26 <166025877+kpranali26@users.noreply.github.com>
2025-01-07 16:37:43 +0000

    basics

---
+++ 01_basics/2.variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountHolder = "pranali"
+var accountPassword = "123456"
+
+accountCity = "jaipur"
+
+// accountId = 2 
+console.log(accountId)
+
+ accountHolder = "heloo"
+ accountPassword = "1456"
+
+ console.table([accountHolder,accountId,accountPassword,accountCity])
---
+++ 01_basics/3.datatypes.js
@@ -0,0 +1,5 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // it is not work in node js ,works in browser
+console.log(typeof"pranali");
+console.log(typeof null);
