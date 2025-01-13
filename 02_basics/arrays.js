8ccd849f68191ba395269885fe06757e93e47877
kpranali26 <166025877+kpranali26@users.noreply.github.com>
2025-01-13 13:14:59 +0000

    .

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -1,31 +1,31 @@
-const arr1 = [1,2,3,4,"pranali","a",];
+const newAr = [1,2,3,4,"pranali","a",];
 const arr0 = [[45,67,["p","h"]],9,8,7]
 const arr2 = new Array(6,7,8,9,10);
-// console.log(arr1);
+// console.log(newArray);
 // console.log(arr2);
-// // console.log(arr1.push(arr2));
-// console.log(arr1);
-// console.log(arr1.pop(arr2));
-// console.log(arr1);
-// console.log(arr1.unshift(2));
-// console.log(arr1);
-// console.log(arr1.shift());
-// console.log(arr1);
-// console.log(arr1.includes(45));
-// console.log(arr1);
-// console.log(arr1.indexOf(2));
-// console.log(arr1);
+// // console.log(newArray.push(arr2));
+// console.log(newArray);
+// console.log(newArray.pop(arr2));
+// console.log(newArray);
+// console.log(newArray.unshift(2));
+// console.log(newArray);
+// console.log(newArray.shift());
+// console.log(newArray);
+// console.log(newArray.includes(45));
+// console.log(newArray);
+// console.log(newArray.indexOf(2));
+// console.log(newArray);
 
 
 //  here slice and splice are most important part according to  the javascript interview:
-    // console.log(arr1.slice(1,4));
-    // console.log(arr1);
-    // console.log(arr1.splice(1,4));
-    // console.log(arr1);
+    // console.log(newArray.slice(1,4));
+    // console.log(newArray);
+    // console.log(newArray.splice(1,4));
+    // console.log(newArray);
 
 
 // Spread operator : again learn it is not working correctly
-const new_arr = {...arr1 , ...arr0};
+const new_arr = {...newArr , ...arr0};
 console.log(new_arr);
 // const res = arr0.flat(Infinity);
 // console.log(res);
@@ -39,6 +39,14 @@ console.log(new_arr);
 // let s3 = 300;
 // console.log("new array", Array.of(s1,s2,s3));
 
+// let s1 = 100 ;
+// let s2 = 200;
+// let s3 = 300;
+// console.log("new array", Array.of(s1,s2,s3));
+// let s1 = 100 ;
+// let s2 = 200;
+// let s3 = 300;
+// console.log("new array", Array.of(s1,s2,s3));
 
 
 
